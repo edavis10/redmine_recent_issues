e6723c7b4b62fe29cf30842835e0430457c57923
Eric Davis <edavis@littlestreamsoftware.com>
2010-09-20 11:42:35 -0700

    Update init.rb about

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_recent_issues do
-  name 'Redmine Recent Issues plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Recent Issues'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-recent-issue'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'Redmine plugin to show the recent issues reported to help prevent duplicate bug reports.'
+  version '0.1.0'
 
   settings :default => {'recent_issue_count' => '5'}, :partial => 'settings/recent_issues'
 end
